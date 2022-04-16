f690c734c1ef12ae618f990c8d8d18e8a6a9803d
gabrielheiwa <heiwa.gabriel@gmail.com>
2022-04-15 21:09:03 -0300

    ADD: check status, remove container and create container of docker server

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DigitalOcean } from './digital_ocean/digital-ocean';
 import { Docker } from './docker/docker';
-import { ManagerService } from './manager/manager.service';
 import { ManagerController } from './manager/manager.controller';
-import { OwnServer } from './own_server';
+import { ManagerService } from './manager/manager.service';
+import { OwnServer } from './own_server/own_server';
 
 @Module({
   imports: [],
--- src/docker/docker.ts
+++ src/docker/docker.ts
@@ -1,28 +1,109 @@
 import { Injectable } from '@nestjs/common';
+import { exec, execSync } from 'child_process';
+
+interface upProps {
+  name: string;
+  image: string;
+  ports: string[];
+  enviroments: string[];
+}
+
+interface downProps {
+  id: string;
+}
+
+interface statusProps {
+  id: string;
+}
 
 @Injectable()
 export class Docker {
-  async up(params) {
-    console.log(params);
+  async up(params: upProps) {
+    const { enviroments, ports, image, name } = params;
+
+    let command = 'docker run -d ';
+    command += `--name ${name}`;
+    command += ` ${this.setup_ports(ports)}`;
+    command += ` ${this.setup_enviroments(enviroments)}`;
+    command += ` ${image}`;
+
+    exec(command, (error, data) => {
+      // If error exist save in databases status server
+      // if (error)
+      //   return {
+      //     message: 'Error to execute this command: ' + command,
+      //     error,
+      //     data,
+      //   };
+      // return {
+      //   message: 'Command make with success!',
+      // };
+    });
 
     return {
-      ev: 'Docker up',
+      message: `Command: "${command}" make with success!`,
     };
   }
 
-  async down(params) {
-    console.log(params);
+  async down(params: downProps) {
+    const { id } = params;
+
+    if (!id)
+      return {
+        error: 'Property "id" is missing',
+      };
+
+    const stop_container = 'docker container stop ' + id;
+    const remove_container = 'docker container rm ' + id;
+    const command = stop_container + ' && ' + remove_container;
+
+    exec(command, (error, data) => {
+      // if (error) return
+      // return
+    });
 
     return {
-      ev: 'Docker down',
+      message: `Command: "${command}" execute with success!`,
     };
   }
 
-  async status(params) {
-    console.log(params);
+  async status(params: statusProps) {
+    const { id } = params;
+
+    if (!id)
+      return {
+        message: 'Property "id" is missing',
+      };
+
+    const output_format = `'{"id":"{{ .ID }}", "image": "{{ .Image }}", "name":"{{ .Names }}"}'`;
+    const command = `docker ps --format ${output_format}`;
+
+    const output = execSync(command);
+    const output_toString = output.toString();
+    const output_stringArray = output_toString.split('\n');
+    const output_jsonArray = output_stringArray
+      .filter((value) => value)
+      .map((value) => JSON.parse(value));
+
+    const find = output_jsonArray.find((container) =>
+      container.id.includes(id),
+    );
+
+    if (find === -1)
+      return {
+        message: 'Container is not runnig',
+      };
 
     return {
-      ev: 'Docker status',
+      message: `Container is running  `,
     };
+  }
+
+  private setup_enviroments(enviroments: string[]): string {
+    return enviroments.map((e) => '-e ' + e).join(' ');
+  }
+
+  private setup_ports(ports: string[]): string {
+    return ports.map((p) => '-p ' + p).join(' ');
   }
 }
